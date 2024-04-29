905b0da4e3e4de6e3acfd1f3fbcfde91170f9f69
StavWasPlayZ <stavsolomon101@gmail.com>
2024-04-29 07:08:48 +0300

    Remove self-registration from GInstrumentMod

--- src/main/java/com/cstav/genshinstrument/GInstrumentMod.java
+++ src/main/java/com/cstav/genshinstrument/GInstrumentMod.java
@@ -1,19 +1,12 @@
 package com.cstav.genshinstrument;
 
 import com.cstav.genshinstrument.attachment.ModAttachments;
-import com.cstav.genshinstrument.client.gui.screen.options.instrument.GridInstrumentOptionsScreen;
 import com.cstav.genshinstrument.item.GIItems;
 import com.cstav.genshinstrument.networking.GIPacketHandler;
 import com.cstav.genshinstrument.sound.GISounds;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.common.Mod.EventBusSubscriber.Bus;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
-import net.neoforged.neoforge.client.ConfigScreenHandler.ConfigScreenFactory;
-import net.neoforged.neoforge.common.NeoForge;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -40,7 +33,5 @@ public class GInstrumentMod
 
         GISounds.register(bus);
         GICreativeModeTabs.regsiter(bus);
-
-        modContainer.getEventBus().register(this);
     }
 }
